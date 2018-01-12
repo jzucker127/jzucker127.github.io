0f0f8f439790eccd397d4425b1f6a691f803dfc1
Jenna Zucker <jennazucker@Jennas-MacBook-Pro.local>
2018-01-12 11:00:11 -0500

    updating my website

---
+++ index.js
@@ -0,0 +1,22 @@
+// call to .ready
+// document is the name of the data structure
+$(document).ready(function() {
+  
+  	// All JS code goes here!
+  	// click is going inside the function
+	// passing a function into a function
+	$('.inline-code').on('click', function() {
+	  $(this).html('wow lol');
+	});
+
+	$('h3').on('mouseover', function() {
+	  $(this).animate({
+	    "letter-spacing": "10px",
+	    "font-size": "40px"
+	  }, 1000).animate({
+	  	"letter-spacing": "5px",
+	  	"font-size": "20px"
+	  }, 1000);
+	});
+  
+});
